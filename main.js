01d0dc15a8f962666170dfcaeb088da410d0b084
GDIsanto <logantom85@gmail.com>
2025-11-27 08:49:26 +0100

    esercizio completato

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('ciao');
+let array = [5, 50, 7, 34, 32, 1, 20, 18];
+
+let media = array.reduce( (acc, el) => acc + el ) / array.length;
+
+console.log(media);
+
+let elementLow = array.filter( el => el < media);
+
+let upLength = array.length - elementLow.length;
+
+console.log(`Media valori = ${media}, elementi minori della media= ${elementLow}, inoltre ci sono ${elementLow.length} elementi inferiori alla media e ${upLength} valori superiori`);
+
+
